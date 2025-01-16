95bcd5d349bd35eda6e63a2bba720306ece76ace
Dave Glassanos <david.glassanos@gmail.com>
2025-01-16 10:30:11 -0800

    Move task queue into workerPool

--- src/api/controllers/BacktestController.ts
+++ src/api/controllers/BacktestController.ts
@@ -1,57 +1,58 @@
 import {type Request, type Response} from 'express'
 import { workerPool } from '../workers/workerPool';
 
-const taskQueue: { req: Request, res: Response }[] = []; // Queue to hold pending requests
-const workers = workerPool.getWorkers(); // Get the worker pool
+// const taskQueue: { req: Request, res: Response }[] = []; // Queue to hold pending requests
+// const workers = workerPool.getWorkers(); // Get the worker pool
 
-// Function to process the next task in the queue
-function processNextTask() {
-  if (taskQueue.length === 0) {
-    return 
-  }
+// // Function to process the next task in the queue
+// function processNextTask() {
+//   if (taskQueue.length === 0) {
+//     return 
+//   }
 
-  if (workers.length === 0) {
-    return 
-  }
+//   if (workers.length === 0) {
+//     return 
+//   }
 
-  const task = taskQueue.shift(); // Get the next task
+//   const task = taskQueue.shift(); // Get the next task
   
-  console.log('Queue depth:', taskQueue.length);
+//   console.log('Queue depth:', taskQueue.length);
 
-  if (!task) {
-    return
-  }
+//   if (!task) {
+//     return
+//   }
 
-  const { req, res } = task;
+//   const { req, res } = task;
 
-  const worker = workers.shift(); // Get an available worker
+//   const worker = workers.shift(); // Get an available worker
 
-  if (!worker) {  
-    throw new Error('No worker available');
-  }
+//   if (!worker) {  
+//     throw new Error('No worker available');
+//   }
 
-  worker.postMessage(req.body);
+//   worker.postMessage(req.body);
 
-  worker.onmessage = (event: MessageEvent) => {
-    workers.push(worker); // Return worker to the pool
+//   worker.onmessage = (event: MessageEvent) => {
+//     workers.push(worker); // Return worker to the pool
 
-    if (event.type === 'error') {
-      res.status(500).send(event.data.message);
-    } else {
-      res.json(event.data); // Send response back to the client
-    }
+//     if (event.type === 'error') {
+//       res.status(500).send(event.data.message);
+//     } else {
+//       res.json(event.data); // Send response back to the client
+//     }
 
-    processNextTask(); // Process the next task in the queue
-  };
+//     processNextTask(); // Process the next task in the queue
+//   };
 
-  worker.addEventListener('close', (event) => {
-    if (event.code !== 0) console.error(`Worker stopped with exit code ${event.code}}`);
-  });
-}
+//   worker.addEventListener('close', (event) => {
+//     if (event.code !== 0) console.error(`Worker stopped with exit code ${event.code}}`);
+//   });
+// }
 
 export default class BacktestController {
     async create(req: Request, res: Response) {
-      taskQueue.push({ req, res }); // Add the request to the queue
-      processNextTask(); // Attempt to process the task
+      workerPool.queue(req, res); 
+      // taskQueue.push({ req, res }); // Add the request to the queue
+      // processNextTask(); // Attempt to process the task
     }
 }
--- src/api/workers/workerPool.ts
+++ src/api/workers/workerPool.ts
@@ -1,8 +1,10 @@
 import os from 'os';
+import {type Request, type Response} from 'express'
 
 class WorkerPool {
     private maxWorkers: number;
-    private workers: Worker[] = []; // Array to hold worker threads
+    private workers: Worker[] = [];
+    private taskQueue: { req: Request, res: Response }[] = [];
 
     constructor() {
         this.maxWorkers = os.cpus().length ?? 1;   
@@ -18,6 +20,55 @@ class WorkerPool {
 
     getWorkers() {
         return this.workers;
+    }
+
+    queue(req: Request, res: Response) {
+        this.taskQueue.push({ req, res });
+        this.processNextTask();
+    }
+
+    processNextTask() {
+        if (this.taskQueue.length === 0) {
+          return 
+        }
+      
+        if (this.workers.length === 0) {
+          return 
+        }
+      
+        const task = this.taskQueue.shift(); // Get the next task
+        
+        console.log('Queue depth:', this.taskQueue.length);
+      
+        if (!task) {
+          return
+        }
+      
+        const { req, res } = task;
+      
+        const worker = this.workers.shift(); // Get an available worker
+      
+        if (!worker) {  
+          throw new Error('No worker available');
+        }
+      
+        worker.postMessage(req.body);
+      
+        worker.onmessage = (event: MessageEvent) => {
+            this.workers.push(worker); // Return worker to the pool
+      
+            if (event.type === 'error') {
+                res.status(500).send(event.data.message);
+            } else {
+                res.json(event.data); // Send response back to the client
+            }
+        
+            this.processNextTask(); // Process the next task in the queue
+        };
+      
+        worker.addEventListener('close', (event) => {
+          if (event.code !== 0) console.error(`Worker stopped with exit code ${event.code}}`);
+        });
     }
 }
 
