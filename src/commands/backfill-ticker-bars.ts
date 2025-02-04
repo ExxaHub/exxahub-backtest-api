6c819790ddfd7911463866087583f6aadb2e4185
Dave Glassanos <david.glassanos@gmail.com>
2025-02-04 10:29:39 -0800

    Fix bug with ticker bar backfill

--- src/commands/backfill-ticker-bars.ts
+++ src/commands/backfill-ticker-bars.ts
@@ -21,7 +21,7 @@ const backfillTicker = async (ticker: string) => {
     const { minDate } = await tickerService.getMaxAndMinDateForTickers([ticker]);
 
     console.log(`Fetching ticker bars for ${ticker} from ${minDate} to today`);
-    const resp = await tiingoClient.getBarsForSymbol(ticker, minDate);
+    const resp = await tiingoClient.getBarsForSymbol(ticker, dayjs(minDate).format('YYYY-MM-DD'));
     const result = await ohlcBarService.bulkInsert(ticker, resp.bars);
     console.log(`Backfilled ticker bars for ${ticker}`);
 }
