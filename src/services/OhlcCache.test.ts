be1bc09401de0fc59c472dbb85751c155d2331bf
Dave Glassanos <david.glassanos@gmail.com>
2025-02-04 09:29:56 -0800

    Add BacktestDateService to clean up date logic

--- src/services/OhlcCache.test.ts
+++ src/services/OhlcCache.test.ts
@@ -20,18 +20,18 @@ mock.module("../repositories/OhlcBarRepository", () => {
       getBarsForDateRange(tickers: string[], fromDate: string, toDate: string) {
         return Promise.resolve({
           'SPY': [
-            { date: "2024-11-10", open: 100, high: 105, low: 95, close: 100, volume: 10 },
-            { date: "2024-11-11", open: 101, high: 106, low: 96, close: 102, volume: 10 },
-            { date: "2024-11-12", open: 102, high: 107, low: 97, close: 104, volume: 10 },
-            { date: "2024-11-13", open: 103, high: 108, low: 98, close: 106, volume: 10 },
-            { date: "2024-11-14", open: 104, high: 109, low: 99, close: 108, volume: 10 },
+            { date: "2024-11-10", close: 100 },
+            { date: "2024-11-11", close: 102 },
+            { date: "2024-11-12", close: 104 },
+            { date: "2024-11-13", close: 106 },
+            { date: "2024-11-14", close: 108 },
           ],
           'QQQ': [
-            { date: "2024-11-10", open: 200, high: 205, low: 195, close: 200, volume: 20 },
-            { date: "2024-11-11", open: 201, high: 206, low: 196, close: 202, volume: 20 },
-            { date: "2024-11-12", open: 202, high: 207, low: 197, close: 204, volume: 20 },
-            { date: "2024-11-13", open: 203, high: 208, low: 198, close: 206, volume: 20 },
-            { date: "2024-11-14", open: 204, high: 209, low: 199, close: 208, volume: 20 },
+            { date: "2024-11-10", close: 200 },
+            { date: "2024-11-11", close: 202 },
+            { date: "2024-11-12", close: 204 },
+            { date: "2024-11-13", close: 206 },
+            { date: "2024-11-14", close: 208 },
           ],
         })
       }
@@ -58,24 +58,24 @@ mock.module("../repositories/OhlcBarSummaryRepository", () => {
 
 describe("OhlcCache", () => {
   it("Loads OHLC bars from client", async () => {
-    const ohlcCache = new OhlcCache(alpacaStockClient, ['SPY', 'QQQ'], 10, 10)
+    const ohlcCache = new OhlcCache(['SPY', 'QQQ'], 10, 10)
 
     await ohlcCache.load(dayjs("2024-11-10"), dayjs("2024-11-14"))
 
     expect(ohlcCache.getBars('SPY')).toEqual([
-      { date: "2024-11-10", open: 100, high: 105, low: 95, close: 100, volume: 10 },
-      { date: "2024-11-11", open: 101, high: 106, low: 96, close: 102, volume: 10 },
-      { date: "2024-11-12", open: 102, high: 107, low: 97, close: 104, volume: 10 },
-      { date: "2024-11-13", open: 103, high: 108, low: 98, close: 106, volume: 10 },
-      { date: "2024-11-14", open: 104, high: 109, low: 99, close: 108, volume: 10 },
+      { date: "2024-11-10", close: 100 },
+      { date: "2024-11-11", close: 102 },
+      { date: "2024-11-12", close: 104 },
+      { date: "2024-11-13", close: 106 },
+      { date: "2024-11-14", close: 108 },
     ])
 
     expect(ohlcCache.getBars('QQQ')).toEqual([
-      { date: "2024-11-10", open: 200, high: 205, low: 195, close: 200, volume: 20 },
-      { date: "2024-11-11", open: 201, high: 206, low: 196, close: 202, volume: 20 },
-      { date: "2024-11-12", open: 202, high: 207, low: 197, close: 204, volume: 20 },
-      { date: "2024-11-13", open: 203, high: 208, low: 198, close: 206, volume: 20 },
-      { date: "2024-11-14", open: 204, high: 209, low: 199, close: 208, volume: 20 },
+      { date: "2024-11-10", close: 200 },
+      { date: "2024-11-11", close: 202 },
+      { date: "2024-11-12", close: 204 },
+      { date: "2024-11-13", close: 206 },
+      { date: "2024-11-14", close: 208 },
     ])
 
     expect(() => ohlcCache.getBars('DIA')).toThrow('Unable to load OHLC bars for ticker: DIA')
